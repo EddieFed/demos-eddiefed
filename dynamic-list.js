cb8abaab272c8d8353a61a5f6c67cef9ec4cd8f5
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2020-02-20 11:47:23 -0600

    Added dynamic list of dirs
    Signed-off-by: Eddie Federmeyer <eddiefedermeyer@yahoo.com>

---
+++ dynamic-list.js
@@ -0,0 +1,27 @@
+function getDemos() {
+    fetch("https://api.github.com/repos/eddiefed/demos-eddiefed/contents/")
+        .then(r => r.json())
+        .then(function(d) {
+            console.log(d);
+            d.forEach((o) => {
+                if (o["type"] === "dir") {
+                    console.log(o["name"], "is a dir");
+                    addListElement(o["name"]);
+                }
+                else {
+                    console.log(o["name"], "is not a dir")
+                }
+            });
+
+        });
+}
+
+function addListElement(dir) {
+    let li = document.createElement("LI");
+    let a = document.createElement("A");
+    a.innerText = dir;
+    a.href = ("./" + dir);
+    li.appendChild(a);
+
+    document.getElementById("list").appendChild(li)
+}
